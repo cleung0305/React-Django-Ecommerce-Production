4493351a7324f91d3504e62047cfc6bbbc72c64c
Lok <loklok12614@gmail.com>
2022-06-30 16:00:28 -0400

    updated

--- frontend/src/services/googleLogin.js
+++ frontend/src/services/googleLogin.js
@@ -1,18 +1,12 @@
 import axios from "axios";
 
-const { REACT_APP_BASE_BACKEND_URL } = process.env;
-
-/** 316802535562-3frd4qtsla2e3vg2rpqrite5b14dh6s5.apps.googleusercontent.com
- * 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
- * This is the accesstoken of the user obtained from Google
- */
 const googleLogin = async ({ data, access_token }) => {
     const headers = {
         Authorization: access_token,
         'Content-Type': 'application/json'
     }
 
-    const res = await axios.post(`${REACT_APP_BASE_BACKEND_URL}/api/users/google/`, data, { headers })
+    const res = await axios.post('/api/users/google/', data, { headers })
     return await res.data
   }
 
